02cf31334cea5232cde7146e23d175157a0763dd
Kojo <info@kojokumah.com>
2015-08-24 16:49:19 -0700

    Added cube and script

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Just a cube here");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
